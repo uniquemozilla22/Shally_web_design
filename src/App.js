cb1e06631e3d7c3bc092f2726fd2384468c2ea54
yogeshbhattarai <yogesh@edigital.edu.np>
2021-12-15 14:00:05 +0545

    Initiation of Shally design

--- src/App.js
+++ src/App.js
@@ -1,46 +1,13 @@
 
-import React, { useState } from 'react';
-import { MDBBtn,
-  MDBModal,
-  MDBModalDialog,
-  MDBModalContent,
-  MDBModalHeader,
-  MDBModalTitle,
-  MDBModalBody,
-  MDBModalFooter,
-} from 'mdb-react-ui-kit';
-
+import React from 'react';
 
 const App= ()=> {
-  const [basicModal, setBasicModal] = useState(false);
-
-  const toggleShow = () => setBasicModal(!basicModal);
-
+  
   return (
     <div className="App">
-    <MDBBtn onClick={()=>toggleShow}>LAUNCH DEMO MODAL</MDBBtn>
-    <MDBModal show={basicModal} setShow={()=>setBasicModal}>
-      <MDBModalDialog>
-        <MDBModalContent>
-          <MDBModalHeader>
-            <MDBModalTitle>Modal title</MDBModalTitle>
-            <MDBBtn className='btn-close' color='none' onClick={toggleShow}></MDBBtn>
-          </MDBModalHeader>
-          <MDBModalBody>...</MDBModalBody>
-
-          <MDBModalFooter>
-            <MDBBtn color='secondary' onClick={toggleShow}>
-              Close
-            </MDBBtn>
-            <MDBBtn>Save changes</MDBBtn>
-          </MDBModalFooter>
-        </MDBModalContent>
-      </MDBModalDialog>
-    </MDBModal>
     </div>
   );
 }
-
 
 
 export default App
