5d30d583e65a75cc3b296e664df4da5b76b597d1
yogeshbhattarai <yogesh@edigital.edu.np>
2021-12-19 19:36:37 +0545

    Recent News Completed

---
+++ src/Components/News-Card/NewsCard.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import classes from './NewsCard.module.css'
+
+
+const NewsCard = ({date,title,description,postedBy,tag,lang ,img}) => {
+    return (
+        <div className={"card "+classes.news_card__container}>
+            <div className={classes.image_background+" bg-image hover-overlay ripple"} data-mdb-ripple-color="dark">
+                <div className={classes.image_container}>
+                    <img
+                        src={img}
+                        className="img-fluid"
+                    />
+                    <span className={classes.title_image}>{lang}</span>
+                </div>
+
+            </div>
+            <div className="card-body">
+                <div className={classes.news_card__details}>
+                    <p className={classes.news_date}>{date}</p>
+                    <p className={classes.news_postedBy}> By {postedBy}</p>
+                    <p className={classes.news_tag}><i className="fa fa-tags"></i> {tag.join(" , ")}</p>
+                </div>
+                <h5 className="card-title">{title}</h5>
+                <p className="card-text">
+                    {description}
+                </p>
+                <a href="#!" className={classes.linking_tag}> Read More  <i class="fas fa-arrow-right"></i></a>
+            </div>
+        </div>
+    )
+}
+
+export default NewsCard
--- src/Components/Recent-News/RecentNews.js
+++ src/Components/Recent-News/RecentNews.js
@@ -1,14 +1,29 @@
 import React from 'react'
+import NewsCard from '../News-Card/NewsCard'
 import "./RecentNews.css"
+import news1 from '../../Assets/news1.jpg'
+import news2 from '../../Assets/news2.jpg'
+import news3 from '../../Assets/news3.jpg'
 
 const RecentNews = () => {
     return (
-        <div clasName="recent_news__section">
+        <div className="recent_news__section">
             <div className="container">
-                <div className="row">
-                    <div className="content__container" >
-                        <h2 className="title">Recent News</h2>
-                        <p className="titles_content"> </p>
+                <div className="content__container" >
+                    <h2 className="title">Recent News</h2>
+                    <p className="titles_content">Nam mattis felis id sodales rutrum. Nulla ornare tristique mauris, a laoreet erat ornare sit amet. Nulla sagittis faucibus lacusMorbi lorem sem, aliquet</p>
+                </div>
+                <div className="news_card__container">
+                    <div className="row">
+                        <div className="col-lg-4 col-md-6 col-xs-12">
+                            <NewsCard img={news1} lang="English" date="12/09/2020" postedBy="Admin" tag={["Teachers", "School"]} title="Campus clean workshop" description="Nam mattis felis id sodales rutrum. Nulla ornare tristique mauris, a laoreet erat ornare sit amet" />
+                        </div>
+                        <div className="col-lg-4 col-md-6 col-xs-12">
+                            <NewsCard img={news2} lang="Nepali" date="12/09/2020" postedBy="Admin" tag={["Teachers", "School"]} title="Campus clean workshop" description="Nam mattis felis id sodales rutrum. Nulla ornare tristique mauris, a laoreet erat ornare sit amet" />
+                        </div>
+                        <div className="col-lg-4 col-md-6 col-xs-12">
+                            <NewsCard img={news3} lang="Spanish" date="12/09/2020" postedBy="Admin" tag={["Teachers", "School"]} title="Campus clean workshop" description="Nam mattis felis id sodales rutrum. Nulla ornare tristique mauris, a laoreet erat ornare sit amet" />
+                        </div>
                     </div>
                 </div>
             </div>
