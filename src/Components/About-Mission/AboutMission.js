98da8031eaf0baf4b27b5af35033af324e58a4de
yogeshbhattarai <yogesh@edigital.edu.np>
2021-12-21 13:10:57 +0545

    VSC

---
+++ src/Components/About-Mission/AboutMission.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { animated, useSpring, useSprings } from 'react-spring'
+import "./AboutMission.css"
+import AboutImage2 from '../../Assets/aboutImage.png'
+
+const AboutMission = () => {
+    const zoomIn = useSpring({
+        loop: false,
+        from: { scale: 0.2 },
+        to: { scale: 1 },
+        delay: 400,
+    })
+    return (
+        <div className="about_mission__section">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-6 col-md-6 col-sm-12">
+                        <animated.div style={zoomIn} className="image__container">
+                            <img src={AboutImage2} alt="About Image 2 "></img>
+                        </animated.div>
+                    </div>
+                    <div className="col-lg-6 col-md-6 col-sm-12">
+                        <div className=""></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default AboutMission
+``
--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Fade, Slide } from 'react-reveal'
+import AboutIntro from '../../Components/About-Intro/AboutIntro'
+import AboutMission from '../../Components/About-Mission/AboutMission'
 import BreadCrumb from '../../Components/BreadCrumb/BreadCrumb'
 
 const About = () => {
     return (
         <main>
+            <Fade cascade bottom>
+                <AboutIntro />
+            </Fade>
+            <Fade cascade bottom>
+                <AboutMission />
+            </Fade>
         </main>
     )
 }
