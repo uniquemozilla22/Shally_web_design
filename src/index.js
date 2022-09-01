c8fd4fdcb59dc23f5f3ff73550b356fbf25c3300
Yogesh Bhattarai <49606627+uniquemozilla22@users.noreply.github.com>
2022-09-01 17:55:16 +0545

    updated the base name of the application

--- src/index.js
+++ src/index.js
@@ -9,9 +9,15 @@ import store from './Redux/store';
 import { BrowserRouter } from 'react-router-dom';
 import "animate.css/animate.min.css";
 
+const baseURL =
+  process.env.NODE_ENV !== "development"
+    ? "/Shally_web_design"
+    : "/shally";
+
+
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={baseURL}>
       <Provider store={store}>
         <App />
       </Provider>
